457d5c5fd9ca43b7ebee2a8d4555dc179651f274
Brittani Taylor <brittani.elan.taylor@gmail.com>
2020-09-10 21:01:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,19 @@
 # Write your code here!
+def game_hash
+  {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => {
+
+      }
+    },
+    :away => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => {
+
+      }
+    }
+  }
+end
